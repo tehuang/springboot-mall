f9595304aa11d96bb480f4f0c7733c7902d37167
Teresa Huang <teresayhhuang@gmail.com>
2024-08-05 19:48:07 -0400

    Product Features: Implement Product Creation

--- src/main/java/com/teresahuang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/teresahuang/springbootmall/dao/ProductDao.java
@@ -1,8 +1,10 @@
 package com.teresahuang.springbootmall.dao;
 
+import com.teresahuang.springbootmall.dto.ProductRequest;
 import com.teresahuang.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/teresahuang/springbootmall/service/ProductService.java
+++ src/main/java/com/teresahuang/springbootmall/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.teresahuang.springbootmall.service;
 
+import com.teresahuang.springbootmall.dto.ProductRequest;
 import com.teresahuang.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
