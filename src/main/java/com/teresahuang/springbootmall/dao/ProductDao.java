d8b1a7fa9a0b147fa0e05c0c1a537608f73369e8
Teresa Huang <teresayhhuang@gmail.com>
2024-08-05 22:58:22 -0400

    Product Features: Implement Query to List All Products

--- src/main/java/com/teresahuang/springbootmall/dao/ProductDao.java
+++ src/main/java/com/teresahuang/springbootmall/dao/ProductDao.java
@@ -3,8 +3,10 @@ package com.teresahuang.springbootmall.dao;
 import com.teresahuang.springbootmall.dto.ProductRequest;
 import com.teresahuang.springbootmall.model.Product;
 
-public interface ProductDao {
+import java.util.List;
 
+public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/teresahuang/springbootmall/service/ProductService.java
+++ src/main/java/com/teresahuang/springbootmall/service/ProductService.java
@@ -3,9 +3,13 @@ package com.teresahuang.springbootmall.service;
 import com.teresahuang.springbootmall.dto.ProductRequest;
 import com.teresahuang.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
     void deleteProductById(Integer productId);
+
 }
