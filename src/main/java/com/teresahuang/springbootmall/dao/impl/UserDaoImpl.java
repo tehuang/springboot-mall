fae372390f8487713b24758f0622f96d1f6f6d63
Teresa Huang <teresayhhuang@gmail.com>
2024-08-09 18:35:14 -0400

    Order Functionality: Unit Test

--- src/main/java/com/teresahuang/springbootmall/dao/impl/UserDaoImpl.java
+++ src/main/java/com/teresahuang/springbootmall/dao/impl/UserDaoImpl.java
@@ -25,7 +25,7 @@ public class UserDaoImpl implements UserDao {
     @Override
     public User getUserById(Integer userId) {
         String sql = "SELECT user_id, email, password, created_date, last_modified_date " +
-                "FROM user WHERE user_id = :userId";
+                "FROM `user` WHERE user_id = :userId";
 
         Map<String, Object> map = new HashMap<>();
         map.put("userId", userId);
@@ -42,7 +42,7 @@ public class UserDaoImpl implements UserDao {
     @Override
     public User getUserByEmail(String email) {
         String sql = "SELECT user_id, email, password, created_date, last_modified_date " +
-                "FROM user WHERE email = :email";
+                "FROM `user` WHERE email = :email";
 
         Map<String, Object> map = new HashMap<>();
         map.put("email", email);
